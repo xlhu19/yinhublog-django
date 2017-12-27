161672d5591323210e4b931c651fe8f4fff81905
xlhu19 <shaul.hu@gmail.com>
2017-12-27 22:58:35 +0800

    fix

---
+++ env.sh
@@ -0,0 +1,9 @@
+
+
+export DJANGO_DATABASE_HOST='127.0.0.1'
+export DJANGO_DATABASE_PORT='5432'
+export DJANGO_DATABASE_NAME='yinhublog2'
+export DJANGO_DATABASE_USER='jingdian'
+export DJANGO_DATABASE_PASSWORD='jingdian'
+
+export DJANGO_SECRET_KEY='t_6#$x^aj_@xslh1al)65*&#9re!ibyex-+=j)e1g8x-ribm)r'
